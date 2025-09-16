fbd4a3eb02f847600230e1ef604f7c51ebf4faa7
DannyM2027 <daniel.matut2027@gbwl.org>
2025-09-16 14:55:35 +0000

    added scanneer

--- Main.java
+++ Main.java
@@ -1,6 +1,31 @@
+// 1. IMPORT SCANNER CLASS
+import java.util.Scanner;
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("Welcome to Madlibs");
+
+      // 2. set up a scanner OBject
+      Scanner input = new Scanner(System.in);
+      // System.in refers to physical keyboard
+
+      // 3. Use the scanner to take input
+      System.out.println("Type ur name then press eneter");
+      String player = input.nextLine();
+      System.out.println("welcome, " + player);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
    }
 }
